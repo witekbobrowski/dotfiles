0f19470a4c1cb5fc8383c7374fd0adb426e1e435
witekbobrowski <witek@bobrowski.com.pl>
2018-03-23 19:59:39 +0100

    Add apps installation.

--- install.sh
+++ install.sh
@@ -1,14 +1,36 @@
 #!/bin/bash
 
-# Create symbolic links for the dotfiles listed in symlink directory
+CURRENT_DIR="$( cd $( dirname "${BASH_SOURCE[0]}" ) && pwd)"
 
-DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )""/symlink" && pwd )"
+# Create symbolic links for the dotfiles listed in symlink directory
+SYMLINK_DIR=$CURRENT_DIR/symlink
 
 dotfiles=(".zshrc"
           ".gitconfig"
           ".phoenix.js")
 
+echo "Creating symbolic links for files from $SYMLINK_DIR"
 for file in "${dotfiles[@]}"; do
   echo "🔗 "$file
-  ln -f $DIR/$file ~/"$file"
+  ln -f $SYMLINK_DIR/$file ~/"$file"
 done
+
+# Install apps
+APPS_DIR=$CURRENT_DIR/apps
+
+which -s brew
+if [[ $? != 0 ]] ; then
+  echo "🛠 Installing homebrew"
+	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+echo "🛠 Installing homebrew apps"
+cat $APPS_DIR/Brewfile | xargs brew install
+echo "🛠 Installing homebrew-cask apps"
+cat $APPS_DIR/Caskfile | xargs brew cask install
+echo "🛠 Installing yarn apps"
+cat $APPS_DIR/Yarnfile | xargs yarn global add
+echo "🛠 Installing gem apps"
+cat $APPS_DIR/Gemfile | xargs sudo gem install
+echo "🛠 Installing Mac App Store apps"
+grep "^[^#]" $APPS_DIR/Masfile | xargs mas install
