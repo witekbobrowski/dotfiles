602bbfe37daed63ae47dabc7dd4ce223dfc01224
witekbobrowski <witek@bobrowski.com.pl>
2018-03-24 12:51:01 +0100

    Update installation script with enhanced output.

--- install.sh
+++ install.sh
@@ -11,7 +11,7 @@ dotfiles=(".zshrc"
 
 echo "Creating symbolic links for files from $SYMLINK_DIR"
 for file in "${dotfiles[@]}"; do
-  echo "🔗 "$file
+  echo "🔗  $file"
   ln -f $SYMLINK_DIR/$file ~/"$file"
 done
 
@@ -20,17 +20,17 @@ APPS_DIR=$CURRENT_DIR/apps
 
 which -s brew
 if [[ $? != 0 ]] ; then
-  echo "🛠 Installing homebrew"
+  echo "🛠  Installing homebrew"
 	/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-echo "🛠 Installing homebrew apps"
+echo "🛠  Installing homebrew apps"
 cat $APPS_DIR/Brewfile | xargs brew install
-echo "🛠 Installing homebrew-cask apps"
+echo "🛠  Installing homebrew-cask apps"
 cat $APPS_DIR/Caskfile | xargs brew cask install
-echo "🛠 Installing yarn apps"
+echo "🛠  Installing yarn apps"
 cat $APPS_DIR/Yarnfile | xargs yarn global add
-echo "🛠 Installing gem apps"
+echo "🛠  Installing gem apps"
 cat $APPS_DIR/Gemfile | xargs sudo gem install
-echo "🛠 Installing Mac App Store apps"
+echo "🛠  Installing Mac App Store apps"
 grep "^[^#]" $APPS_DIR/Masfile | xargs mas install
