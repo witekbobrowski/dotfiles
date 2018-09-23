6c2194f5c203f3c5bc05a665c670d5c73d7a9de3
Witek Bobrowski <witek@bobrowski.com.pl>
2018-09-23 23:18:31 +0200

    Update defaults script for macOS

--- defaults/macos.sh
+++ defaults/macos.sh
@@ -1,45 +1,53 @@
 #!/bin/bash
 
+emoji="🎚    "
+
+log() {
+    prefix="$1"
+    message="$2"
+    echo "$prefix$message"
+}
+
 # macOS Config
-echo "Configuring macOS"
+log "$emoji" "Configuring macOS"
 
 # Change users shell to zsh
-echo "🛠 Setting default shell to zsh"
+log "$emoji" "Setting default shell to zsh"
 chsh -s /bin/zsh
 
 # Save screenshots to the Screenshots folder in Downloads
-echo "🛠 Setting location for screenshots to ~/Downloads/Screenshots/"
-defaults write com.apple.screencapture location -string "${HOME}/Downloads/Screenshots"
+log "$emoji" "Setting location for screenshots to ~/Pictures/Screenshots/"
+defaults write com.apple.screencapture location -string "${HOME}/Pictures/Screenshots"
 
 # Declutter desktop from icons for hard drives, servers, and removable media
-echo "🛠 Hiding icons for hard drives, servers and removable media on desktop"
+log "$emoji" "Hiding icons for hard drives, servers and removable media on desktop"
 defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false
 defaults write com.apple.finder ShowMountedServersOnDesktop -bool false
 defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false
 
 # Disable Dashboard
-echo "🛠 Disabling Dashboard"
+log "$emoji" "Disabling Dashboard"
 defaults write com.apple.dashboard mcx-disabled -bool true
 
 # Set the icon size of Dock items to 32 pixels
-echo "🛠 Setting Dock size to 32"
+log "$emoji" "Setting Dock size to 32"
 defaults write com.apple.dock tilesize -int 32
 
 # Don’t show Dashboard as a Space
-echo "🛠 Don’t show Dashboard as a Space"
+log "$emoji" "Don’t show Dashboard as a Space"
 defaults write com.apple.dock dashboard-in-overlay -bool true
 
 # Don’t automatically rearrange Spaces based on most recent use
-echo "🛠 Don’t automatically rearrange Spaces based on most recent use"
+log "$emoji" "Don’t automatically rearrange Spaces based on most recent use"
 defaults write com.apple.dock mru-spaces -bool false
 
 # Bottom left screen corner → Start screen saver
-echo "🛠 Set bottom left screen corner as screen saver hotkey"
+log "$emoji" "Set bottom left screen corner as screen saver hotkey"
 defaults write com.apple.dock wvous-bl-corner -int 5
 defaults write com.apple.dock wvous-bl-modifier -int 0
 
 # Bottom right screen corner → Put display to sleep
-echo "🛠 Set bottom right screen corner as put to display to sleep hotkey"
+log "$emoji" "Set bottom right screen corner as put to display to sleep hotkey"
 defaults write com.apple.dock wvous-br-corner -int 10
 defaults write com.apple.dock wvous-br-modifier -int 0
